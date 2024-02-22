4f82b44e2420fd94f0ed2b70f840462b3b4a012a
Six66 <antonandrade259@gmail.com>
2024-02-22 08:35:18 -0600

    Yaaaaaaaaaaaaaaaaa?.

--- NetBeansProjects/Conversor_Unidad/src/main/java/Logica/Formulas.java
+++ NetBeansProjects/Conversor_Unidad/src/main/java/Logica/Formulas.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Logica;
 
-/**
- *
- * @author kevin
- */
 public class Formulas {
     protected double Valor1,Valor2;
     protected String texto1,texto2;
